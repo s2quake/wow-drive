95f9164f27f252b0185fa6578a8f0caed5217ee9
s2quake <s2quake@naver.com>
2021-01-03 21:49:38 +0900

    애드온 해제 경로 관련 에러 수정

--- unzip-addons.ps1
+++ unzip-addons.ps1
@@ -2,7 +2,7 @@ Import-Module "./common.psm1"
 
 try {
     $addonsPath = Get-AddonsPath
-    $backupPath = Join-Path $installPath "Interface/AddOns-new"
+    $backupPath = "$addonsPath-new"
     $zipPath = Get-LatestZipPath (Get-OneDrivePath) "AddOns"
     
     Expand-Archive -LiteralPath $zipPath -DestinationPath $backupPath
